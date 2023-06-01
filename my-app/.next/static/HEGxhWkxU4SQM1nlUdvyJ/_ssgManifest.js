7eb8f512f6a46cb6b9e675ac0b78fac43ebb903d
Yfd_omibeaver <108644167+winterOmii@users.noreply.github.com>
2023-06-01 13:27:44 +0800

    up

--- my-app/.next/static/HEGxhWkxU4SQM1nlUdvyJ/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002FAboutus","\u002FContactPage","\u002Fcategory\u002F[categoryId]","\u002FproductSearch\u002F[ProductSearchId]","\u002Fproduction\u002F[productId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
